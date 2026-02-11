09bd7970a56e797d8c19ee8624398359955ac1dd
Julliane <dipaulajulliane@gmail.com>
2026-02-10 23:05:27 -0300

    :sparkles: feat: Footer

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,26 +1,25 @@
-import Link from "next/link"
-import { Button } from "../ui/button"
-import { ActiveLink } from "../active-link";
+import Link from 'next/link';
+import { ActiveLink } from '../active-link';
+import { Button } from '../ui/button';
+import Image from 'next/image';
 
 export const Header = () => {
-
-  return(
-    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-80">
+  return (
+    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 ls:px-80">
         <div className="flex h-16 items-center justify-between">
-          <Link href="/">Logo</Link>
-
+           <Link href='/'>
+            <Image src="/logo.svg" alt="Logo" width={116} height={32}/>
+          </Link>
           <nav className="flex items-center gap-6">
-            <ActiveLink href='/'>Início</ActiveLink>
-            <ActiveLink href="/blog" >Blog</ActiveLink>
-            <Button variant={"secondary"} asChild>
-              <Link href='/comecar'>
-                Começar
-              </Link>
+            <ActiveLink href="/">Início</ActiveLink>
+            <ActiveLink href="/blog">Blog</ActiveLink>
+            <Button variant="secondary" asChild>
+              <Link href="/comecar">Começar</Link>
             </Button>
           </nav>
         </div>
       </div>
     </header>
-  )
-}
+  );
+};
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,16 +1,16 @@
-import { Header } from "../header"
+import { Footer } from '../footer';
+import { Header } from '../header';
 
 type LayoutProps = {
   children: React.ReactNode;
-}
+};
 
 export const Layout = ({ children }: LayoutProps) => {
-  return(
+  return (
     <div className="relative flex min-h-screen flex-col dark">
-      <Header/>
-      <main className="flex-1 flex flex-col mb-12">
-        {children}
-      </main>
+      <Header />
+      <main className="flex-1 flex flex-col mt-10 mb-12">{children}</main>
+      <Footer/>
     </div>
-  )
-}
+  );
+};
