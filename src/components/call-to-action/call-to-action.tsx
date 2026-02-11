6849267538864a50db1ce7350d783783538f5fce
Julliane <dipaulajulliane@gmail.com>
2026-02-11 12:37:23 -0300

    :lipstick: style: Ajustando as fontes

--- src/components/call-to-action/call-to-action.tsx
+++ src/components/call-to-action/call-to-action.tsx
@@ -1,12 +1,6 @@
 import { ArrowRight, Store } from 'lucide-react';
-import { PT_Sans_Caption } from 'next/font/google';
 import Link from 'next/link';
 import { Button } from '../ui/button';
-
-const ptSansCaption = PT_Sans_Caption({
-  subsets: ['latin'],
-  weight: '700',
-});
 
 export const CallToAction = () => {
   return (
@@ -18,7 +12,7 @@ export const CallToAction = () => {
           </div>
 
           <h2
-            className={`${ptSansCaption.className} text-gray-100 text-balance text-heading-xl`}
+            className="font-sans text-gray-100 text-balance text-heading-xl"
           >
             Crie uma loja online e inicie suas vendas ainda hoje
           </h2>
