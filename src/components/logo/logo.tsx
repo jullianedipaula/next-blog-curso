d2a2b87d0ef3f98e3a99403a388cbb495333d75f
Julliane <dipaulajulliane@gmail.com>
2026-02-10 23:11:13 -0300

    :sparkles: feat: Logo

---
+++ src/components/logo/logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export const Logo = () => {
+  return(
+
+    <Link href='/' title="Página Inicial">
+      <Image src="/logo.svg" alt="Logo" width={116} height={32}/>
+    </Link>
+
+  )
+}
