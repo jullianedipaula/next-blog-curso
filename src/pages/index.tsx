87c156829c98ca3439798d6abcb3a1b67f5568ab
Julliane <dipaulajulliane@gmail.com>
2026-02-11 12:20:44 -0300

    :sparkles: feat: Call to action Section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { CallToAction } from '@/components/call-to-action'
 import { CustomerStorySection } from '@/components/customer-story-section'
 import { FeatureSection } from '@/components/feature-section'
 import { HeroSection } from '@/components/hero-section'
@@ -5,13 +6,12 @@ import { SupportSection } from '@/components/support-section'
 
 export default function Home() {
   return (
-    <>
       <article className="flex flex-col">
         <HeroSection />
         <FeatureSection/>
         <SupportSection />
         <CustomerStorySection />
+        <CallToAction/>
       </article>
-    </>
   )
 }
