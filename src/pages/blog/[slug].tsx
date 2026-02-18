55c7d02e1198ff0fbb23791ec52a37e649df2aea
Julliane <dipaulajulliane@gmail.com>
2026-02-18 14:59:44 -0300

    :sparkles: feat: Componente: Avatar

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,7 +1,7 @@
-import { useRouter } from 'next/router';
-import Image from 'next/image';
-import Link from 'next/link';
-import { allPosts } from 'contentlayer/generated';
+import { useRouter } from 'next/router'
+import Image from 'next/image'
+import Link from 'next/link'
+import { allPosts } from 'contentlayer/generated'
 
 import {
   Breadcrumb,
@@ -9,14 +9,22 @@ import {
   BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
+} from '@/components/ui/breadcrumb'
+import { Avatar } from '@/components/avatar'
 
 export default function PostPage() {
-  const router = useRouter();
-  const slug = router.query.slug as string;
-  const post = allPosts.find((post) =>
-    post.slug.toLowerCase().includes(slug.toLowerCase())
-  );
+  const router = useRouter()
+  const { slug } = router.query
+
+  const post = slug 
+    ? allPosts.find((post) => post.slug.toLowerCase() === (slug as string).toLowerCase())
+    : null
+
+  if (!post) {
+    return null 
+  }
+
+  const publishedDate = new Date(post.date).toLocaleDateString('pt-BR')
 
   return (
     <main className="mt-32 text-gray-100">
@@ -44,8 +52,29 @@ export default function PostPage() {
               className="object-cover"
             />
           </figure>
+
+          <header className="p-4 md:p-6 lg:p-12 pb-0">
+            <h1 className="mb-6 text-balance text-heading-lg md:text-heading-xl lg:text-heading-xl">
+              {post?.title}
+            </h1>
+
+            <Avatar.Container>
+              <Avatar.Image
+                  src={post?.author.avatar}
+                  alt={post?.title}
+                  size="sm"
+                />
+              <Avatar.Content>
+                <Avatar.Title>{post?.author.name}</Avatar.Title>
+                <Avatar.Description>
+                  Publicado em {''}
+                  <time dateTime={post.date}>{publishedDate}</time>
+                </Avatar.Description>
+              </Avatar.Content>
+            </Avatar.Container>
+          </header>
         </article>
       </div>
     </main>
-  );
+  )
 }
